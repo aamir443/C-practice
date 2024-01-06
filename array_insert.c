ca4e89061f6139e09ee7adb505471dc2884b2431
aamir930 <mohammadaamir2002@gmail.com>
2024-01-06 23:31:02 +0530

    insert element in array

---
+++ array_insert.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+    {
+        int a[100],n,i,item;
+        printf("eneter size of array ");
+        scanf("%d",&n);
+        printf("enter element in array ");
+        for(i=0;i<n;i++){
+        scanf("%d",&a[i]);
+        }
+        printf("insert an element at begining ");
+        scanf("%d",&item);
+        n++;
+        for(i=n;i>1;i--)
+        {
+            a[i-1]=a[i-2];
+        }
+        a[0]=item;
+        printf("resultant array ");
+        for(i=0;i<5;i++)
+        printf("%d",a[i]);
+        return 0;
+}
