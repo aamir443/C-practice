0aeb2e55aff88d5ca15a61d5f200aeef1d0b5322
aamir930 <mohammadaamir2002@gmail.com>
2024-01-07 17:42:10 +0530

    string paractice

---
+++ string_reverse.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    char str[1000], reversedStr[1000];
+    int i, j;
+    printf("Enter a string: ");
+    scanf("%[^\n]",str);
+    for (i = 0; str[i] != '\0'; i++) {
+    }
+    for (j = 0; i >= 0; i--, j++) {
+        reversedStr[j] = str[i - 1];
+    }
+    reversedStr[j] = '\0';
+    printf("Reversed string: %s\n", reversedStr);
+    return 0;
+}
