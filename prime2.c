2aeab19da2a4fe9b3fff5ef47591abb39084ce65
aamir930 <mohammadaamir2002@gmail.com>
2024-02-10 00:23:46 +0530

    c

--- prime2.c
+++ prime2.c
@@ -1,23 +1,44 @@
+// #include<stdio.h>
+// void checkPrime(int n);
+// int main()
+// {
+//     int n;
+//     printf("Enter a number : ");
+//     scanf("%d",&n);
+//     checkPrime(n);
+//     return 0;
+// }
+// void checkPrime(int n)
+// {
+//     int i,cf=0;
+//     for(i=1;i<=n;i++)
+//     {    
+//         if(n%i==0)
+//         cf++;
+//     }
+//     if(cf==2)
+//     printf("%d prime number",n);
+//     else
+//     printf("%d not prime number",n);
+// }
 #include<stdio.h>
-void checkPrime(int n);
 int main()
 {
-    int n;
-    printf("Enter a number : ");
-    scanf("%d",&n);
-    checkPrime(n);
-    return 0;
-}
-void checkPrime(int n)
-{
-    int i,cf=0;
-    for(i=1;i<=n;i++)
-    {    
-        if(n%i==0)
-        cf++;
+    int i, count=0, j;
+    printf("Prime numbers between 1 to 100 are:\n");
+    for(i=1; i<=100; i++)
+    {
+        for(j=2; j<i; j++)
+        {
+           if(i%j==0)
+           {
+               count++;
+               break;
+           }
+        }
+        if(count==0 && i!=1)
+            printf("%d\n", i);
+        count = 0;
     }
-    if(cf==2)
-    printf("%d prime number",n);
-    else
-    printf("%d not prime number",n);
+    return 0;
 }
